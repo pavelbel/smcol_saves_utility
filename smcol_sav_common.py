2ad3383721540a79fd88703a67ea3c5dac194512
pavelbel <124062579+pavelbel@users.noreply.github.com>
2024-06-01 00:54:50 +0300

    Prime resources placement algo func added
    Reverse engineered and coded by Nick Wagers (nawagers). Taken from: https://github.com/nawagers/Colonization-SAV-files
    (Suppressed and sea/water checks removed)
    
    Co-Authored-By: Nick Wagers <33252537+nawagers@users.noreply.github.com>

--- smcol_sav_common.py
+++ smcol_sav_common.py
@@ -1,5 +1,23 @@
 import json
 
+
+def is_prime(row, col, prime_resource_seed):
+    """Prime resources placement algo. Reverse engineered and coded by Nick Wagers (nawagers).
+      Taken from: https://github.com/nawagers/Colonization-SAV-files
+      Suppressed and sea/water checks removed."""
+
+    pattern = {0: [0, 10, 17, 27, 34, 40, 51, 57],
+               1: [4, 14, 21, 31, 38, 44, 55, 61],
+               2: [2, 8, 19, 25, 32, 42, 49, 59],
+               3: [6, 12, 23, 29, 36, 46, 53, 63]}
+    col += 4 * prime_resource_seed + (row // 4) * 12
+    clean_val = col % 64 in pattern[row % 4]
+    col += 60
+    forest_val = col % 64 in pattern[row % 4]
+
+    return clean_val, forest_val
+
+
 def load_settings(settings_json_filename: str, default_settings: dict):
 
     if default_settings is None:
@@ -15,6 +33,7 @@ def load_settings(settings_json_filename: str, default_settings: dict):
         settings['colonize_path'] = default_settings['colonize_path']
 
     return settings
+
 
 def get_input(input_hint: str, res_type=str, check_fun=lambda x: True, error_str=''):
     """Ввод значения c клавиатуры с преобразованием в нужный тип и повторами в случае некорректных значений"""
